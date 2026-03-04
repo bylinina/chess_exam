17f36017da93d45cd87583d0985cd4400e919f6e
bylinina <53174684+bylinina@users.noreply.github.com>
2026-03-04 22:58:47 +0100

    Rename validate import to validate_player

--- chess_tournament/__init__.py
+++ chess_tournament/__init__.py
@@ -1,4 +1,4 @@
 from .players import Player, RandomPlayer, EnginePlayer, LMPlayer, SmolPlayer
 from .game import Game
 from .tournament import run_tournament, round_robin_tournament, swiss_tournament, instantiate_participant, destroy_instance
-from .validate import validate
+from .validate import validate_player
